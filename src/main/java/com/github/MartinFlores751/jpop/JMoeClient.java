ec6e6d93af3a6833b16f928bf0fe28df7e1923c5
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2020-08-30 20:41:01 -0500

    Fix heartbeat crash, format

--- src/main/java/com/github/MartinFlores751/jpop/JMoeClient.java
+++ src/main/java/com/github/MartinFlores751/jpop/JMoeClient.java
@@ -4,7 +4,6 @@ import com.github.MartinFlores751.events.CurrentSongEvent;
 import org.greenrobot.eventbus.EventBus;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,24 +40,31 @@ public class JMoeClient extends WebSocketClient {
     public void onMessage(String message) {
         logger.debug(message);
         JsonObject obj = Json.createReader(new StringReader(message)).readObject();
-        switch(obj.getInt("op")) {
-            case 0: int heartbeat = obj.getJsonObject("d").getInt("heartbeat");
+        switch (obj.getInt("op")) {
+            case 0:
+                int heartbeat = obj.getJsonObject("d").getInt("heartbeat");
                 heartbeatTimer.scheduleAtFixedRate(new HeartbeatTask(), heartbeat, heartbeat);
                 break;
             case 1:
-                EventBus.getDefault().post( new CurrentSongEvent(obj.getJsonObject("d").getJsonObject("song")));
-            case 10: logger.debug("Good heartbeat");
+                EventBus.getDefault().post(new CurrentSongEvent(obj.getJsonObject("d").getJsonObject("song")));
+            case 10:
+                logger.debug("Good heartbeat");
                 break;
         }
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        heartbeatTimer.cancel();
+
     }
 
     @Override
     public void onError(Exception ex) {
         logger.error(ex.toString());
+    }
+
+    public void stop() {
+        heartbeatTimer.cancel();
+        close();
     }
 }
