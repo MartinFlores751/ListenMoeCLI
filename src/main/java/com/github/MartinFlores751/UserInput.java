178ad51f18ca11f7caaf93d106f0897bd1a05241
Martin Jaime Flores Jr <martin.flores98@tutanota.com>
2020-08-30 20:42:01 -0500

    Add logger, doc for helper function, minor refactor

--- src/main/java/com/github/MartinFlores751/UserInput.java
+++ src/main/java/com/github/MartinFlores751/UserInput.java
@@ -5,10 +5,13 @@ import com.googlecode.lanterna.input.InputProvider;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import org.greenrobot.eventbus.EventBus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class UserInput implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(UserInput.class);
     private final InputProvider term;
     private boolean isQuit = false;
 
@@ -16,12 +19,17 @@ public class UserInput implements Runnable {
         this.term = term;
     }
 
+    /**
+     * Helper function to get user input
+     * @return Returns current user input, may be null
+     */
     private KeyStroke getInput() {
-        KeyStroke res = null;
+        KeyStroke res;
         try {
             res = term.readInput();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.warn("Failed to read user input!", e);
+            return null;
         }
         return res;
     }
